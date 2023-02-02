050ffd23c2a1a48839d770a997d34404a51975ce
Akul <akuldpatel16@gmail.com>
2023-02-02 15:48:14 -0500

    second

---
+++ Desktop/javascript accordian/accordian.js
@@ -0,0 +1,13 @@
+function hideDisplaySection(section)
+{
+    var target = document.getElementById(section);
+
+    if(target.style.display == "block")
+    {
+        target.style.display = "none";
+    }
+    else
+    {
+        target.style.display = "block";
+    }
+}
